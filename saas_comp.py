779f8648fec5d62c4c9ef540d874dbbb89d0ed72
Denis <denis_kuza2001@mail.ru>
2022-09-23 23:23:54 +0300

    request json encoding complete

--- saas_comp.py
+++ saas_comp.py
@@ -2,36 +2,21 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import base64
+import json
 
-st.title('Uber pickups in NYC')
+#encoding to base64 format
+def encode_file(file):
+    file_content = file.read()
+    return base64.b64encode(file_content)
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+#updating body.json
+def make_json_body(file):
+    base64_file = encode_file(file)
+    return {"folderId":"b1gb2477drh0ivbugf0p", "analyze_specs": [{"content":f"{str(base64_file)}", "features":[{"type": "FACE_DETECTION"}]}]}
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+with open ('./data/mask.jpg', 'rb') as img:
+    with open ("body.json", 'w', encoding='utf-8') as json_f:
+        res_json = make_json_body(img)
+        json.dump(make_json_body(img), json_f)
