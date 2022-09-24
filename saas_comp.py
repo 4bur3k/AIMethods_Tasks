bec339436b46a38c0b1e2675fe8f594f8a89e0ce
Denis <denis_kuza2001@mail.ru>
2022-09-24 21:27:10 +0300

    detection work!

--- saas_comp.py
+++ saas_comp.py
@@ -14,7 +14,7 @@ def encode_file(file):
 
 #dumps image to json in format that api need
 def make_json_body():
-    with open ('./data/face-detection-sample.jpg', 'rb') as img:
+    with open ('./data/mask.jpg', 'rb') as img:
         base64_file = encode_file(img)
         data = {"folderId":"b1gb2477drh0ivbugf0p", "analyze_specs": [{"content":base64_file.decode(), "features":[{"type": "FACE_DETECTION"}]}]}
 
@@ -29,12 +29,12 @@ data = json.dumps(make_json_body())
 print(data)
 header = {"Authorization": f"Api-Key {API_KEY}"}
 
-#response = requests.post("https://vision.api.cloud.yandex.net/vision/v1/batchAnalyze", json=data, headers=header)
-#print(f'Response code:{response}')
-#resoult = response.json()
-#print(resoult)
+response = requests.post("https://vision.api.cloud.yandex.net/vision/v1/batchAnalyze", json=data, headers=header)
+print(f'Response code:{response}')
+resoult = response.json()
+print(resoult)
 
-#with open('output.json', 'w') as res_f:
-    #detected_faces = json.dump(resoult, res_f)
+with open('output.json', 'w') as res_f:
+    detected_faces = json.dump(resoult, res_f)
 
 
