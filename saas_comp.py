322ab5605f1020c237dabe46943ae87d9c44e131
Denis <denis_kuza2001@mail.ru>
2022-09-24 19:35:39 +0300

    interim resoult

--- saas_comp.py
+++ saas_comp.py
@@ -4,18 +4,32 @@ import pandas as pd
 import numpy as np
 import base64
 import json
+import requests
 
 #encoding to base64 format
 def encode_file(file):
     file_content = file.read()
     return base64.b64encode(file_content)
 
-#updating body.json
-def make_json_body(file):
-    base64_file = encode_file(file)
-    return {"folderId":"b1gb2477drh0ivbugf0p", "analyze_specs": [{"content":base64_file.decode(), "features":[{"type": "FACE_DETECTION"}]}]}
+#dumps image to json in format that api need
+def make_json_body():
+    with open ('./data/mask.jpg', 'rb') as img:
+        base64_file = encode_file(img)
+        data = {"folderId":"b1gb2477drh0ivbugf0p", "analyze_specs": [{"content":base64_file.decode(), "features":[{"type": "FACE_DETECTION"}]}]}
+        return json.dumps(data)
 
+#making POST request
+f = open("yandex_tkn.txt", 'r', encoding='utf-8')
+API_KEY = f.read()
 
-with open ('./data/mask.jpg', 'rb') as img:
-    with open ("body.json", 'w', encoding='utf-8') as json_f:
-        json.dump(make_json_body(img), json_f)
+json_f = open ("body.json", 'r')
+data = json.loads(json_f.read())
+header = {"Authorization": f"Api-Key {API_KEY}"}
+
+response = requests.post("https://vision.api.cloud.yandex.net/vision/v1/batchAnalyze", json=data, headers=header)
+resoult = response.json()
+print(f'Response code:{response}')
+print(resoult)
+
+with open('output.txt', 'w') as res_f:
+    detected_faces = json.dump(resoult, res_f)
