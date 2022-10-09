43e2fe422f76cbd3b835d72ffc0ab1440d7f6023
Denis <denis_kuza2001@mail.ru>
2022-10-09 16:14:32 +0300

    version for test

--- task_2/Scripts/main.py
+++ task_2/Scripts/main.py
@@ -1,3 +1,4 @@
+from json import load
 import numpy as np
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
@@ -29,34 +30,42 @@ def generate(
   return list(map(tok.decode, out))
 
 
-tok, model = load_tokenizer_and_model("sberbank-ai/rugpt3small_based_on_gpt2")
-generated = generate(model, tok, "Аннотация к теме \"Комбинаторное описание обобщенных чисел Гурвица\"", num_beams=10)
+model, tok = load_tokenizer_and_model("sberbank-ai/rugpt3small_based_on_gpt2")
+
+output_string = "Here you will see generated text"
+
+def init_model(model_name):
+  if model_name == 'Small':
+    model, tok = load_tokenizer_and_model("sberbank-ai/rugpt3small_based_on_gpt2")
+  else :
+    model, tok = load_tokenizer_and_model("sberbank-ai/rugpt2large")
+  
 
 
-print(generated[0])
 
-# output_string = "Here you will see generated text"
+def generate_button_cklick():
+    print('Genrating... ', model, max_words)
+    generated = generate(model, tok, "Аннотация к теме \"Комбинаторное описание обобщенных чисел Гурвица\"", num_beams=10)
+    output_string = generated[0]
 
-# def generate_button_cklick():
-#     print(model, max_words)
 
 
-# import streamlit as st
+import streamlit as st
 
-# st.title('ruGPT-3 generator')
 
-# col1, col2 = st.columns([2, 3])
+st.title('ruGPT-3 generator')
 
-# with col1:
-#     st.subheader('Params')
-#     text = st.text_input('Print any phrase')
-#     model = st.radio('Choose the model', ('Small', 'Medium'))
-#     max_words  = st.slider('Max words number', min_value= 10, max_value= 120, value=40)
-#     generate = st.button('Generate', on_click=generate_button_cklick)
+col1, col2 = st.columns([2, 3])
+
+with col1:
+    st.subheader('Params')
+    text = st.text_input('Print any phrase')
+    model_name = st.radio('Choose the model', ('Small', 'Medium'), on_change=init_model)
+    max_words  = st.slider('Max words number', min_value= 10, max_value= 120, value=40)
+    st.button('Generate', on_click=generate_button_cklick)
 
     
  
-# with col2:
-#     st.subheader('Resoult')
-#     st.text_area(label='Generated text', value=output_string, height=300)
-
+with col2:
+    st.subheader('Resoult')
+    st.text_area(label='Generated text', value=output_string, height=300)
